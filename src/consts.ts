f97d95723d18fb20bb1fc50f6775126ffc271f17
Teodoro <tedtke514@gmail.com>
2024-06-05 11:59:21 -0500

    updated basic components and implemented contact form logic

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Ubuntu';
+export const SITE_TITLE = 'Hey Teo Lets Code';
 export const SITE_DESCRIPTION = 'Welcome to my website!';
